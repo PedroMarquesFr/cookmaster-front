81d6b323105b8de81a0b985deb33f388b4df1375
Pedro Marques Fr <pedromarques1011@gmail.com>
2021-03-28 09:54:20 -0300

    startl login screen

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
 
 // import { Container } from './styles';
 
 const Login: React.FC = () => {
+  const [login, setLogin] = useState({ email: "", password: "" });
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = () => {
+    const { email, password } = login;
+    if (email.length < 7 && password.length < 7) {
+      return setMessage("Dados invalidos");
+    }
+  };
+
   return (
     <div>
-      Login <Link to="/register">Register</Link>
+      <span>{message}</span>
+      <label htmlFor="email">Email:</label>
+      <input
+        id="email"
+        onChange={({ target: { value } }) =>
+          setLogin({ ...login, email: value })
+        }
+      />
+      <label htmlFor="password">Password:</label>
+      <input
+        id="password"
+        onChange={({ target: { value } }) =>
+          setLogin({ ...login, password: value })
+        }
+      />
+      <button onClick={handleSubmit}>Login</button>
+      <Link to="/register">Register</Link>
     </div>
   );
 };
